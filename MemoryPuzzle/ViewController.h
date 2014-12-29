8dc1abd8a4b6f3299025c5d71538fef2614f4b5c
slyao <Days@shangjiafengdeMacBook-Pro.local>
2014-12-29 21:52:06 +0800

    Initial Commit

---
+++ MemoryPuzzle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryPuzzle
+//
+//  Created by slyao on 14/12/29.
+//  Copyright (c) 2014年 slyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MemoryPuzzle/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MemoryPuzzle
+//
+//  Created by slyao on 14/12/29.
+//  Copyright (c) 2014年 slyao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
